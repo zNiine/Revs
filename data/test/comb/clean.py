a6500c2310f4170f4f4e4c5e4dcf0bbcbb3f1a33
zNiine <idkconflict1@gmail.com>
2025-07-10 13:22:41 -0400

    new data

---
+++ data/test/comb/clean.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+# List of columns to remove
+cols_to_drop = [
+    "DetectedShift",
+    "1B_PositionAtReleaseX", "1B_PositionAtReleaseZ",
+    "2B_PositionAtReleaseX", "2B_PositionAtReleaseZ",
+    "3B_PositionAtReleaseX", "3B_PositionAtReleaseZ",
+    "SS_PositionAtReleaseX", "SS_PositionAtReleaseZ",
+    "LF_PositionAtReleaseX", "LF_PositionAtReleaseZ",
+    "CF_PositionAtReleaseX", "CF_PositionAtReleaseZ",
+    "RF_PositionAtReleaseX", "RF_PositionAtReleaseZ",
+    "1B_Name", "1B_Id",
+    "2B_Name", "2B_Id",
+    "3B_Name", "3B_Id",
+    "SS_Name", "SS_Id",
+    "LF_Name", "LF_Id",
+    "CF_Name", "CF_Id",
+    "RF_Name", "RF_Id",
+    "FHC",
+    "PitchReleaseConfidence", "PitchLocationConfidence", "PitchMovementConfidence",
+    "HitLaunchConfidence", "HitLandingConfidence",
+    "CatcherThrowCatchConfidence", "CatcherThrowReleaseConfidence", "CatcherThrowLocationConfidence",
+    "ThrowTrajectoryXc0", "ThrowTrajectoryXc1", "ThrowTrajectoryXc2",
+    "ThrowTrajectoryYc0", "ThrowTrajectoryYc1", "ThrowTrajectoryYc2",
+    "ThrowTrajectoryZc0", "ThrowTrajectoryZc1", "ThrowTrajectoryZc2"
+]
+
+# Read in the full dataset
+df = pd.read_csv("data.csv")
+
+# Drop unwanted columns (errors='ignore' skips any cols not present)
+df = df.drop(columns=cols_to_drop, errors='ignore')
+
+# Write out the cleaned data
+df.to_csv("data_clean.csv", index=False)
+
+print("Saved cleaned CSV to data_clean.csv")
