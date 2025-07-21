e2208d3cb0cc6e526e4bd79bc69da11bae430b4f
akendrick2-MGH <akendrick2@mgh.harvard.edu>
2025-07-21 15:16:46 -0400

    new data
    new data

--- csv/yes.py
+++ csv/yes.py
@@ -7,10 +7,10 @@ df = pd.read_csv('../data.csv')  # <-- replace with your actual filename
 pitching_df = df[df['PitcherTeam'] == 'YOR']
 
 # Filter for rows where the batter team is "YOR"
-batting_df = df[df['BatterTeam'] == 'HAG_FLY']
+batting_df = df[df['BatterTeam'] == 'STA_YAN']
 
 # Save each filtered DataFrame to CSV
-pitching_df.to_csv('pitching_HP.csv', index=False)
-batting_df.to_csv('batting_YOR.csv', index=False)
+pitching_df.to_csv('pitching_YOR.csv', index=False)
+batting_df.to_csv('batting_STAN.csv', index=False)
 
 print("✅ Done! Files saved as 'pitching_HP.csv' and 'batting_YOR.csv'")
